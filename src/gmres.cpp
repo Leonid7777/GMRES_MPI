2eec9cff95eda94e288ebf21c488d238f126700a
Leonid Burtsev <l.bustaev@s02210338@gse.cs.msu.ru>
2024-04-30 13:47:18 +0300

    H_vec

--- src/gmres.cpp
+++ src/gmres.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include "vec_methods.h"
 #include "mat_methods.h"
+#include <iostream>
 
 
 void
@@ -11,10 +12,10 @@ GMRES(int size_of_matrix, double* matrix, double* right_part, double* res)
     int krylov_count = 0;
 
     double* krylov_subspaces = new double[(size_of_matrix + 1) * size_of_matrix];
-    double* H = new double[(size_of_matrix + 1) * size_of_matrix];
 
     int kr_v = std::max(2, (size_of_matrix + 1) / 10);
     std::vector<double> Q_vec ((size_of_matrix + 1) * kr_v);
+    std::vector<double> H_vec ((size_of_matrix + 1) * (kr_v - 1));
 
     eye_mat_place(Q_vec, size_of_matrix + 1, kr_v, 0);
 
@@ -32,36 +33,37 @@ GMRES(int size_of_matrix, double* matrix, double* right_part, double* res)
             int place = kr_v;
             kr_v = std::min(2 * kr_v, size_of_matrix + 1);
             Q_vec.resize(kr_v * (size_of_matrix + 1));
+            H_vec.resize((kr_v - 1) * (size_of_matrix + 1));
             eye_mat_place(Q_vec, size_of_matrix + 1, kr_v, place);
         }
 
         mat_vec(matrix, krylov_subspaces + krylov_count * size_of_matrix, krylov_subspaces + (krylov_count + 1) * size_of_matrix, size_of_matrix, size_of_matrix);
 
         for(int i = 0; i <= krylov_count; i++) {
-            H[krylov_count * (size_of_matrix + 1) + i] = vec_to_vec(krylov_subspaces + (krylov_count + 1) * size_of_matrix, krylov_subspaces + i * size_of_matrix, size_of_matrix);
+            H_vec[krylov_count * (size_of_matrix + 1) + i] = vec_to_vec(krylov_subspaces + (krylov_count + 1) * size_of_matrix, krylov_subspaces + i * size_of_matrix, size_of_matrix);
         }
 
         for(int i = 0; i <= krylov_count; i++) {
-            vec_sub_vec(krylov_subspaces + (krylov_count + 1) * size_of_matrix, krylov_subspaces + i * size_of_matrix, size_of_matrix, H[krylov_count * (size_of_matrix + 1) + i]);
+            vec_sub_vec(krylov_subspaces + (krylov_count + 1) * size_of_matrix, krylov_subspaces + i * size_of_matrix, size_of_matrix, H_vec[krylov_count * (size_of_matrix + 1) + i]);
         }
 
-        H[krylov_count * (size_of_matrix + 1) + krylov_count + 1] = vec_norm(krylov_subspaces + (krylov_count + 1) * size_of_matrix, size_of_matrix);
+        H_vec[krylov_count * (size_of_matrix + 1) + krylov_count + 1] = vec_norm(krylov_subspaces + (krylov_count + 1) * size_of_matrix, size_of_matrix);
 
-        if(H[krylov_count * (size_of_matrix + 1) + krylov_count + 1] == 0) {
+        if(H_vec[krylov_count * (size_of_matrix + 1) + krylov_count + 1] == 0) {
             krylov_count++;
             break;
         } else {
-            vec_del(krylov_subspaces + (krylov_count + 1) * size_of_matrix, size_of_matrix, H[krylov_count * (size_of_matrix + 1) + krylov_count + 1]);
+            vec_del(krylov_subspaces + (krylov_count + 1) * size_of_matrix, size_of_matrix, H_vec[krylov_count * (size_of_matrix + 1) + krylov_count + 1]);
         }
 
-        place_mat_vec(Q_vec, H + krylov_count * (size_of_matrix + 1), krylov_count + 1, size_of_matrix + 1);
+        place_mat_vec(Q_vec, &H_vec[krylov_count * (size_of_matrix + 1)], krylov_count + 1, size_of_matrix + 1);
 
-        denom = std::sqrt(H[krylov_count * (size_of_matrix + 1) + krylov_count] * H[krylov_count * (size_of_matrix + 1) + krylov_count] + H[krylov_count * (size_of_matrix + 1) + krylov_count + 1] * H[krylov_count * (size_of_matrix + 1) + krylov_count + 1]);
+        denom = hypotenuse(H_vec[krylov_count * (size_of_matrix + 1) + krylov_count], H_vec[krylov_count * (size_of_matrix + 1) + krylov_count + 1]);
 
-        c =  H[krylov_count * (size_of_matrix + 1) + krylov_count] / denom;
-        s =  H[krylov_count * (size_of_matrix + 1) + krylov_count + 1] / denom;
+        c =  H_vec[krylov_count * (size_of_matrix + 1) + krylov_count] / denom;
+        s =  H_vec[krylov_count * (size_of_matrix + 1) + krylov_count + 1] / denom;
 
-        both_position_mat_mul(H, Q_vec, size_of_matrix + 1, krylov_count + 1, krylov_count, c, s);
+        both_position_mat_mul(H_vec, Q_vec, size_of_matrix + 1, krylov_count + 1, krylov_count, c, s);
 
         err = std::abs(Q_vec[krylov_count + 1]);
         krylov_count++;
@@ -72,9 +74,9 @@ GMRES(int size_of_matrix, double* matrix, double* right_part, double* res)
     for(int i = krylov_count - 1; i >= 0; i--) {
         y[i] = Q_vec[i] * norm;
         for(int j = i + 1; j < krylov_count; j++) {
-            y[i] -= H[j * (size_of_matrix + 1) + i] * y[j];
+            y[i] -= H_vec[j * (size_of_matrix + 1) + i] * y[j];
         }
-        y[i] /=  H[i * (size_of_matrix + 1) + i];
+        y[i] /=  H_vec[i * (size_of_matrix + 1) + i];
     }
 
     for(int i = 0; i < krylov_count; i++) {
@@ -84,6 +86,5 @@ GMRES(int size_of_matrix, double* matrix, double* right_part, double* res)
     }
 
     delete[] krylov_subspaces;
-    delete[] H;
     delete[] y;
 }
--- src/mat_methods.cpp
+++ src/mat_methods.cpp
@@ -10,7 +10,7 @@ eye_mat_place(std::vector<double>& mat, int n, int m, int place)
 }
 
 void 
-both_position_mat_mul(double* res, std::vector<double>& vec_res, int n, int m, int position, double c, double s)
+both_position_mat_mul(std::vector<double>& res, std::vector<double>& vec_res, int n, int m, int position, double c, double s)
 {
     double val_1;
 
